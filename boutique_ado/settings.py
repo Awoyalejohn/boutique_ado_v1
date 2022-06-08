b00f65a7f040453acebe0bd57ca23b67c71c43f5
John Awoyale <awoyalejohn@gmail.com>
2022-06-08 19:47:17 +0100

    [200~Django E commerce Allauth Setup Part 1

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -73,8 +73,7 @@ TEMPLATES = [
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
+    'allauth.account.auth_backends.AuthenticationBackend'
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
