9e1f0991e38af761687f8b90eff257f54526122b
Awoyalejohne <awoyalejohn@gmail.com>
2022-06-26 01:13:33 +0100

    Add checkout success logic

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from requests import delete
 
 from .models import OrderLineItem
 
@@ -11,8 +12,9 @@ def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
+    print('delete signal received')
     instance.order.update_total()
